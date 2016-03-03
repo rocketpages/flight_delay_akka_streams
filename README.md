84a959a2ea72a7da823c69c7fcc561340ee52d33
Kevin Webber <kevin@kevinwebber.ca>
2016-03-03 16:48:00 -0500

    Minor updates

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
-Akka streams activator template
-===============================
+# Flight Delay Streaming
 
-[![Build Status](https://travis-ci.org/typesafehub/activator-akka-stream-scala.svg?branch=master)](https://travis-ci.org/typesafehub/activator-akka-stream-scala)
+An Akka Streams application that crunches flight data from a CSV file and emits average delays for each airline carrier.
+Akka Streams will ensure that you're never late for an important business trip again!
 
-Demonstrate Akka streams in Scala.
+1. Convert each line from a String into a FlightEvent
+2. Filter out non-delayed flights from the stream
+3. Broadcast the stream across two distinct flows — one to capture raw events, another to capture aggregate flight delay data
+4. Emit a substream per airline carrier, accumulating the total number of delayed flights and the minutes of each delay for each airline, then merging the streams together with the totals
+5. Print flight delay information to the console
 
-Akka Streams is an implementation of [Reactive Streams](http://www.reactive-streams.org/),
-which is a standard for asynchronous stream processing with non-blocking backpressure.
+To run this app, you will need to visit http://stat-computing.org/dataexpo/2009/the-data.html and download the CSV file for
+a single year. This file should be placed in src/main/resources.
 
--- src/main/scala/sample/stream/FlightDelayStreaming.scala
+++ src/main/scala/sample/stream/FlightDelayStreaming.scala
@@ -2,6 +2,50 @@ package sample.stream
 
 import akka.NotUsed
 
+/**
+
+        FlowShape
+      Outlet[String]                  FlowShape[String, FlightEvent]         [FlightEvent, FlightDelayRecord]
+ ╔══════════════════════╗                ╔══════════════════════╗                ╔══════════════════════╗
+ ║                      ║                ║                      ║                ║                      ║
+ ║                      ║                ║                      ║                ║                      ║
+ ║                      ╠──┐          ┌──╣                      ╠──┐          ┌──╣                      ╠──┐
+ ║  Source              │  │─────────▶│  │   csvToFlightEvent   │  │─────────▶│  │   filterAndConvert   │  │──┐
+ ║     .fromIterator()  ╠──┘          └──╣                      ╠──┘          └──╣                      ╠──┘  │
+ ║                      ║                ║                      ║                ║                      ║     │
+ ║                      ║                ║                      ║                ║                      ║     │
+ ║                      ║                ║                      ║                ║                      ║     │
+ ╚══════════════════════╝                ╚══════════════════════╝                ╚══════════════════════╝     │
+                                                                                                              │
+                                                                                                              │
+                                                                                      UniformFanOutShape      │
+                                                 Inlet[Any]       [FlightDelayRecord, FlightDelayRecord]      │
+                                         ╔══════════════════════╗                ╔══════════════════════╗     │
+                                         ║                      ║                ║                      ║     │
+                                         ║                      ║                ║                      ║     │
+                                         ║                     ┌╩─┐           ┌──╣                      ╠──┐  │
+                                         ║      Sink.ignore    │  │◀──────────│  │      Broadcast       │  │◀─┘
+                                         ║                     └╦─┘           └──╣                      ╠──┘
+                                         ║                      ║                ║                      ║
+                                         ║                      ║                ║                      ║
+                                         ║                      ║                ║                      ║
+                                         ╚══════════════════════╝                ╚═════════╦──╦═════════╝
+                                                                                           │  │
+                                                                                           └──┘
+                                                       FlowShape                             │
+      Inlet[Any]         [FlightDelayRecord, (String, Int, Int)]                             │
+╔══════════════════════╗                ╔══════════════════════╗                             │
+║                      ║                ║                      ║                             │
+║                      ║                ║                      ║                             │
+║                      ╠──┐          ┌──╣                      ╠──┐                          │
+║     Sink.            │  │◀─────────│  │  averageCarrierDelay │  │◀─────────────────────────┘
+║       foreach(_)     ╠──┘          └──╣                      ╠──┘
+║                      ║                ║                      ║
+║                      ║                ║                      ║
+║                      ║                ║                      ║
+╚══════════════════════╝                ╚══════════════════════╝
+  */
+
 object FlightDelayStreaming {
   import akka.actor.ActorSystem
   import akka.stream._
@@ -28,21 +72,24 @@ object FlightDelayStreaming {
 
         // Flows
         val B: FlowShape[String, FlightEvent] = builder.add(csvToFlightEvent)
-        val C: FlowShape[FlightEvent, FlightDelayRecord] = builder.add(flightEventToDelayRecord)
+        val C: FlowShape[FlightEvent, FlightDelayRecord] = builder.add(filterAndConvert)
         val D: UniformFanOutShape[FlightDelayRecord, FlightDelayRecord] = builder.add(Broadcast[FlightDelayRecord](2))
-        val F: FlowShape[FlightDelayRecord, (String, Int, Int)] = builder.add(countByCarrier)
+        val F: FlowShape[FlightDelayRecord, (String, Int, Int)] = builder.add(averageCarrierDelay)
 
         // Sinks
         val E: Inlet[Any] = builder.add(Sink.ignore).in
         val G: Inlet[Any] = builder.add(Sink.foreach(averageSink)).in
 
         // Graph
-        A ~> B ~> C ~> D ~> E
-                       D ~> F ~> G
+        A ~> B ~> C ~> D
+                  E <~ D
+             G <~ F <~ D
 
         ClosedShape
-    }).run()
+    })
     // @formatter:on
+
+    g.run()
 
   }
 
@@ -64,19 +111,18 @@ object FlightDelayStreaming {
   def stringArrayToFlightEvent(cols: Array[String]) = new FlightEvent(cols(0), cols(1), cols(2), cols(3), cols(4), cols(5), cols(6), cols(7), cols(8), cols(9), cols(10), cols(11), cols(12), cols(13), cols(14), cols(15), cols(16), cols(17), cols(18), cols(19), cols(20), cols(21), cols(22), cols(23), cols(24), cols(25), cols(26), cols(27), cols(28))
 
   // transform FlightEvents to DelayRecords (only for records with a delay)
-  val flightEventToDelayRecord =
+  val filterAndConvert =
     Flow[FlightEvent]
       .filter(r => Try(r.arrDelayMins.toInt).getOrElse(-1) > 0) // convert arrival delays to ints, filter out non delays
       .mapAsyncUnordered(parallelism = 2) { r =>
         Future(new FlightDelayRecord(r.year, r.month, r.dayOfMonth, r.flightNum, r.uniqueCarrier, r.arrDelayMins))
       } // output a FlightDelayRecord
 
-  val countByCarrier =
+  val averageCarrierDelay =
     Flow[FlightDelayRecord]
       .groupBy(30, _.uniqueCarrier)
       .fold(("", 0, 0)) {
         (x: (String, Int, Int), y: FlightDelayRecord) =>
-          //println(s"Delays for carrier ${y.uniqueCarrier}: ${Try(x._3 / x._2).getOrElse(0)} average mins, ${x._2} delayed flights") // TODO emit as events for another graph, perhaps event storage + SSE
           val count = x._2 + 1
           val totalMins = x._3 + Try(y.arrDelayMins.toInt).getOrElse(0)
           (y.uniqueCarrier, count, totalMins)
